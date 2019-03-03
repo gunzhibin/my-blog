0f18887ddfc4df87cf34cb36d239012b92fa9dcd
gunzhibin <gunzhibin@zuoyebang.com>
2019-03-03 19:26:02 +0800

    提交master

---
+++ src/components/AddBlog.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="add-blog">
+  <h1>AddBlog</h1>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "add-blog"
+    }
+</script>
+
+<style scoped>
+
+</style>
